0e14a3359773f7c03eb0d1d5dd67bac5d68bc264
NikitaKx9 <nikitakx9@gmail.com>
2023-01-21 08:57:41 +0300

    Доработка задачи 1 и 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,6 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2 - 4*a*c;
-  console.log(d);
   if (d < 0) {
     arr = [];
   } else if ( d === 0) {
@@ -16,12 +15,20 @@ function solveEquation(a, b, c) {
   return arr;
 
 }
+console.log(solveEquation(1, 5, 4));
+console.log(solveEquation(1, 2, 1));
+console.log(solveEquation(1, 2, 10));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let mounthPercent = percent/100*12;
+  if (percent !== 0 || 100) {
+    alert('Неверно указана процентная ставка')
+  }
+  let mounthPercent = percent/(100*12);
   let body = amount - contribution;
   let pay = body * (mounthPercent + (mounthPercent / (((1 + mounthPercent) ** countMonths) - 1)));
   let fullPay = +(pay * countMonths).toFixed(2);
-  typeof(fullPay);
+  console.log(fullPay);
   return fullPay;
 }
+
+calculateTotalMortgage(10, 0, 50000, 12);
