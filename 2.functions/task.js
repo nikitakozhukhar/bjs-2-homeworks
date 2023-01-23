c56434273c302d1a7d63161b894e7511cd88a4b0
NikitaKx9 <nikitakx9@gmail.com>
2023-01-23 04:41:48 +0300

    Функции начал задачу №1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,23 @@
 function getArrayParams(...arr) {
-
+  let min = arr[0];
+  let max = arr[0];
+  let sum;
+  let avg;
+  for (let i = 0; i <= arr; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      sum += max;
+      console.log(sum)
+    } else if (arr[i] < min) {
+      min = arr[i];
+      sum += min;
+      console.log(sum)
+    }
+    avg = sum / arr.length;
+  }
   return { min: min, max: max, avg: avg };
 }
+getArrayParams([-99, 99, 10])
 
 function summElementsWorker(...arr) {
 
