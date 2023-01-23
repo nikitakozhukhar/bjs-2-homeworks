dcdbc763a4f49f6fb055518d57f9b2cf149e2fd5
NikitaKx9 <nikitakx9@gmail.com>
2023-01-23 13:21:38 +0300

    2 var of tast #1. Function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,23 +1,26 @@
 function getArrayParams(...arr) {
-  let min = arr[0][0];
-  let max = arr[0][0];
+  /*let min = arr[0];
+  let max = arr[0];
   let sum = 0;
   let avg;
-  for (let i = 0; i < arr[0].length; i++) {
-    if (arr[0][i] > max) {
-      max = arr[0][i];
-    } else if (arr[0][i] < min) {
-      min = arr[0][i];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
     }
-    sum += arr[0][i];
-    avg = +(sum / arr[0].length).toFixed(2);
-  }
-  
+    sum += arr[i];
+    avg = +(sum / arr.length).toFixed(2);
+  }*/
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  let sum = arr.reduce(function(a , b) {
+    return a + b;
+  });
+  let avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
-
-getArrayParams([-99, 99, 10]);
-
+getArrayParams(-99, 99, 10);
 function summElementsWorker(...arr) {
 
 }
