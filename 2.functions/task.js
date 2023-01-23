b2291ed4eb9e6a2ec24b2b3079c4b89cc419d892
NikitaKx9 <nikitakx9@gmail.com>
2023-01-23 11:32:04 +0300

    Function task #1.1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,23 +1,22 @@
 function getArrayParams(...arr) {
-  let min = arr[0];
-  let max = arr[0];
-  let sum;
+  let min = arr[0][0];
+  let max = arr[0][0];
+  let sum = 0;
   let avg;
-  for (let i = 0; i <= arr; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-      sum += max;
-      console.log(sum)
-    } else if (arr[i] < min) {
-      min = arr[i];
-      sum += min;
-      console.log(sum)
+  for (let i = 0; i < arr[0].length; i++) {
+    if (arr[0][i] > max) {
+      max = arr[0][i];
+    } else if (arr[0][i] < min) {
+      min = arr[0][i];
     }
-    avg = sum / arr.length;
+    sum += arr[0][i];
+    avg = +(sum / arr[0].length).toFixed(2);
   }
+  
   return { min: min, max: max, avg: avg };
 }
-getArrayParams([-99, 99, 10])
+
+getArrayParams([-99, 99, 10]);
 
 function summElementsWorker(...arr) {
 
