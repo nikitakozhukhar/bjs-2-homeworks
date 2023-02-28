46167d7a008f346ba011c8b443f15c543c7667c8
NikitaKx9 <nikitakx9@gmail.com>
2023-02-28 16:40:29 +0300

    Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,41 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cash = {};
+
+  return (...args) => {
+	const hash = args.join(',');
+	if (hash in cash) {
+		return cash[hash]
+	}
+
+	const result = func(...args);
+	cash[hash] = result;
+	return result;
+  }
 }
 
+sum = (...args) => args.reduce((acc, item) => acc + item, 0);
+
+const cashedSum = cachingDecoratorNew(sum);
+debugger;
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   
 }
+
+
+function debounceDecoratorNew(func, delay) {
+	let timeoutId = null;
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+  
+	function wrapper(...args) {
+	  
+	}
+  
+	return wrapper;
+  }
+  
+  /*module.exports = {
+	debounceDecoratorNew, 
+  }*/
