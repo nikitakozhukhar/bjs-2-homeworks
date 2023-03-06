7823e889b225ad88e4e03000e27d4edf8301a159
NikitaKx9 <nikitakx9@gmail.com>
2023-03-06 21:44:50 +0300

    Декораторы v1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -2,46 +2,44 @@
 function cachingDecoratorNew(func) {
   let cash = {};
 
-  return (...args) => {
-	const hash = args.join(',');
-	if (hash in cash) {
-		return cash[hash]
-	}
+  return function(...args) {
+		const hash = md5(args);
+		if (hash in cash) {
+			return `Из кэша: ${cash[hash]}`
+		}
 
-	const result = func(...args);
-	cash[hash] = result;
-	return result;
+		const result = func(...args);
+		cash[hash] = result;
+		return `Вычмсляем: ${result}`;
   }
 }
 
-// sum = (...args) => args.reduce((acc, item) => acc + item, 0);
-
-// const cashedSum = cachingDecoratorNew(sum);
-// debugger;
-
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
+// function debounceDecoratorNew(func, delay) {
   
-}
-
+// }
 
 function debounceDecoratorNew(func, delay) {
 	let timeoutId = null;
 	wrapper.count = 0;
 	wrapper.allCount = 0;
-  
+	
 	function wrapper(...args) {
-		wrapper.history.push(args)
-		return func(...args)
+		if (!timeoutId){
+			console.log(func(...args))
+		} 
+		timeoutId = setTimeout(() => {
+			clearTimeout(timeoutId);
+			console.log(func(...args));
+		}, delay)
+		// console.log(args)
+		wrapper.allCount++
 	}
-	wrapper.history = [];
-	console.log(a, b)
+	if (func) {
+		wrapper.count++;
+	}
 	return wrapper;
-  }
-//   const sum = (a, b) => a + b;
-
-//   let spy = debounceDecoratorNew(sum)
-  
+}
   /*module.exports = {
 	debounceDecoratorNew, 
   }*/
