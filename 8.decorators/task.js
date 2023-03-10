539b19653414b772bcbbda5502c25c2b89190d97
NikitaKx9 <nikitakx9@gmail.com>
2023-03-10 05:46:58 +0300

    Декораторы v5

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,12 +5,14 @@ function cachingDecoratorNew(func) {
 	console.log(cash.hash)
    
 	function wrapper(...args) {
-		const hash = (args);
+		const hash = md5(args);
 		if (hash in cash) {
 			console.log(`Из кэша ${hash}`);
 			return `Из кэша: ${cash[hash]}`
 		}
 
+		// cash.push({hash: md5(args), value: func(...args)});
+		console.log(cash)
 		const result = func(...args);
 		cash[hash] = result;
 		console.log(`Вычисляем ${result}`);
@@ -33,16 +35,20 @@ function debounceDecoratorNew(func, delay) {
 	function wrapper(...args) {
 		if (!timeoutId){
 			console.log(func(...args))
+			wrapper.count++
 		} 
 		timeoutId = setTimeout(() => {
 			clearTimeout(timeoutId);
 			console.log(func(...args));
+			console.log(wrapper.count);
+			wrapper.count++;
 		}, delay)
 		// console.log(args)
 		wrapper.allCount++
+		// console.log(wrapper.allCount);
 	}
 	if (func) {
-		wrapper.count++;
+		// wrapper.count++;
 	}
 	return wrapper;
 }
